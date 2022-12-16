20d1e2999ccac11a1c794991607026c24c1bea0b
Frederico Dôndici Gama Vieira <fredericodgv@gmail.com>
2022-12-16 18:31:01 -0300

    Adiciona metodo deleteBooks

--- src/app.js
+++ src/app.js
@@ -2,18 +2,47 @@ import express from "express"
 
 const app = express();
 
-const livros = [
-    {id: 1, "titulo:": "Senhor dos Aneis"},
-    {id: 2, "titulo:": "O Hobbit"},
+app.use(express.json());
+
+const books = [
+    {id: 1, "title": "Senhor dos Aneis"},
+    {id: 2, "title": "O Hobbit"},
 ]
 
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
+app.get("/books", (req, res) => {
+    res.status(200).json(books);
 });
+
+app.get("/books/:id", (req, res) => {
+    let index = searchBook(req.params.id);
+    res.json(books[index]);
+});
+
+app.post("/books", (req, res) => {
+    books.push(req.body);
+    res.status(201).send("Livro adicionado com sucesso!");
+});
+
+app.put("/books/:id", (req, res) => {
+    let index = searchBook(req.params.id);
+    books[index].title = req.body.title;
+    res.json(books);
+});
+
+app.delete("/books/:id", (req, res) => {
+    let {id} = req.params;
+    let index = searchBook(id);
+    books.splice(index, 1);
+    res.send("Livro removido com sucesso!");
+});
+
+function searchBook(id) {
+    return books.findIndex(book => book.id == id);
+}
 
 
 export default app
